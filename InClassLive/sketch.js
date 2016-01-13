f094d0b0347f9b2dda8c41bea35021491a50e4bc
Francisco Dias <frandias@gmail.com>
2016-01-12 20:21:17 -0500

    showed console.log to debug

--- InClassLive/sketch.js
+++ InClassLive/sketch.js
@@ -1,8 +1,11 @@
 function setup() {
   createCanvas(500,500)
-  console.log("Hello World")
+  console.log("Hello World again")
 }
 
 function draw() {
-  background(255,0,0)
+  background(255,0,0);
+  console.log("another world");
+  background(34,23,0);
+  console.log("another world didn't show up");;
 }
